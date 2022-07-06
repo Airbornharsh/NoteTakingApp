8b6b97f9b4e707db43b985fef1c0d847c44427ad
Harsh Keshri <harshkeshri1234567@gmail.com>
2022-07-06 11:06:19 +0530

    Enabling CORS

--- notes/services/util/handler.js
+++ notes/services/util/handler.js
@@ -1,21 +1,25 @@
 export default function handler(lambda) {
-    return async function (event, context) {
-      let body, statusCode;
-  
-      try {
-        // Run the Lambda
-        body = await lambda(event, context);
-        statusCode = 200;
-      } catch (e) {
-        console.error(e);
-        body = { error: e.message };
-        statusCode = 500;
-      }
-  
-      // Return HTTP response
-      return {
-        statusCode,
-        body: JSON.stringify(body),
-      };
+  return async function (event, context) {
+    let body, statusCode;
+
+    try {
+      // Run the Lambda
+      body = await lambda(event, context);
+      statusCode = 200;
+    } catch (e) {
+      console.error(e);
+      body = { error: e.message };
+      statusCode = 500;
+    }
+
+    // Return HTTP response
+    return {
+      statusCode,
+      body: JSON.stringify(body),
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Credentials": true,
+      },
     };
-  }
+  };
+}
--- notes/stacks/StorageStack.js
+++ notes/stacks/StorageStack.js
@@ -2,7 +2,16 @@ import { Bucket, Table } from "@serverless-stack/resources";
 
 export function StorageStack({ stack, app }) {
   // Create an S3 bucket
-  const bucket = new Bucket(stack, "Uploads");
+  const bucket = new Bucket(stack, "Uploads", {
+    cors: [
+      {
+        maxAge: "1 day",
+        allowedOrigins: ["*"],
+        allowedHeaders: ["*"],
+        allowedMethods: ["GET", "PUT", "POST", "DELETE", "HEAD"],
+      },
+    ],
+  });
 
   // Create the DynamoDB table
   const table = new Table(stack, "Notes", {
