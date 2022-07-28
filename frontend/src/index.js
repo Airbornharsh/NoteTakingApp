ea700712377d5f93f9192c1ffeb98cd84ad33310
Harsh Keshri <harshkeshri1234567@gmail.com>
2022-07-28 09:30:05 +0530

    addded some auth Changes

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router } from "react-router-dom";
-import { Amplify } from "aws-amplify";
+import { Amplify, Auth } from "aws-amplify";
 import config from "./config";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.css";
@@ -9,6 +9,48 @@ import App from "./App";
 import ContextProvider from "./Context/ContextProvider";
 
 Amplify.configure({
+  Auth: {
+    mandatorySignIn: true,
+    region: config.cognito.REGION,
+    userPoolId: config.cognito.USER_POOL_ID,
+    identityPoolId: config.cognito.IDENTITY_POOL_ID,
+    userPoolWebClientId: config.cognito.APP_CLIENT_ID,
+    oauth: {
+      domain: `${
+        "notetakingapp-not" +
+        ".auth." +
+        config.cognito.REGION +
+        ".amazoncognito.com"
+      }`,
+      scope: [
+        "phone",
+        "email",
+        "profile",
+        "openid",
+        "aws.cognito.signin.user.admin",
+      ],
+      redirectSignIn: "http://localhost:3000",
+      redirectSignOut: "http://localhost:3000",
+      responseType: "token",
+    },
+  },
+  Storage: {
+    region: config.s3.REGION,
+    bucket: config.s3.BUCKET,
+    identityPoolId: config.cognito.IDENTITY_POOL_ID,
+  },
+  API: {
+    endpoints: [
+      {
+        name: "notes",
+        endpoint: config.apiGateway.URL,
+        region: config.apiGateway.REGION,
+      },
+    ],
+  },
+});
+
+Auth.configure({
   Auth: {
     mandatorySignIn: true,
     region: config.cognito.REGION,
