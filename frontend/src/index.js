cde04d0ee1da42e6030aa5cc2ae8818c2298ad88
Harsh Keshri <harshkeshri1234567@gmail.com>
2022-07-30 14:54:03 +0530

    Revert "removed auth configure"
    This reverts commit 505485df2738802fecb6be219a16e3d39c97c6fe.

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -50,6 +50,48 @@ Amplify.configure({
   },
 });
 
+Auth.configure({
+  Auth: {
+    mandatorySignIn: true,
+    region: config.cognito.REGION,
+    userPoolId: config.cognito.USER_POOL_ID,
+    identityPoolId: config.cognito.IDENTITY_POOL_ID,
+    userPoolWebClientId: config.cognito.APP_CLIENT_ID,
+    oauth: {
+      domain: `${
+        "notetakingapp-not" +
+        ".auth." +
+        config.cognito.REGION +
+        ".amazoncognito.com"
+      }`,
+      scope: [
+        "phone",
+        "email",
+        "profile",
+        "openid",
+        "aws.cognito.signin.user.admin",
+      ],
+      redirectSignIn: "http://localhost:3000",
+      redirectSignOut: "http://localhost:3000",
+      responseType: "token",
+    },
+  },
+  Storage: {
+    region: config.s3.REGION,
+    bucket: config.s3.BUCKET,
+    identityPoolId: config.cognito.IDENTITY_POOL_ID,
+  },
+  API: {
+    endpoints: [
+      {
+        name: "notes",
+        endpoint: config.apiGateway.URL,
+        region: config.apiGateway.REGION,
+      },
+    ],
+  },
+});
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
